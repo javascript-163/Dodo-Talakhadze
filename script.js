66b00ca0ea25f6a068a73e50e67c86e15373c505
DodoTalakhadze <dtalakhadze2@gmail.com>
2024-01-23 20:40:32 +0400

    correct

--- script.js
+++
@@ -1,6 +0,0 @@
-let name = prompt("სახელი"); 
-let surname = prompt("გვარი"); 
-let birthYear = prompt("დაბადების წელი"); 
-let address = prompt("საცხოვრებელი ადგილი"); 
-let age = 2024-birthYear; 
-alert("გამარჯობა "+ name + " " + surname + ", მიხარია რომ უკვე " + age + " წლის ხარ და " + address + " აქ ცხოვრობ");
