8983d239bda38a6f774b3175dd79dfbdf7051c70
DodoTalakhadze <dtalakhadze2@gmail.com>
2024-01-26 22:27:55 +0400

    this is homework 3

--- script.js
+++ script.js
@@ -1,55 +1,55 @@
-// // task 1 
-// function square(number){
-//     return number * number;
-// }
-// let number = 5;
-// let result = square(number);
-// console.log('the square of', number, 'is', result);
+// task 1 
+function square(number){
+    return number * number;
+}
+let number = 5;
+let result = square(number);
+console.log('the square of', number, 'is', result);
 
 
-// // task 2
-// function CelToFar(celsius){
-//     return (celsius * 9/5) + 32;
-// }
-// let celsiusTemp = 25;
-// let farTemp = CelToFar(celsiusTemp);
-// console.log(celsiusTemp + ' celsius is ' + farTemp + ' farenheit');
+// task 2
+function CelToFar(celsius){
+    return (celsius * 9/5) + 32;
+}
+let celsiusTemp = 25;
+let farTemp = CelToFar(celsiusTemp);
+console.log(celsiusTemp + ' celsius is ' + farTemp + ' farenheit');
 
-// // task 3 
-// function numberRange(number, firstNUm, lastNum){
-//     return (number >= firstNUm && number <= lastNum)
-// };
-// let userNumber = 50;
-// let firstNUm = 10;
-// let lastNum = 80;
+// task 3 
+function numberRange(number, firstNUm, lastNum){
+    return (number >= firstNUm && number <= lastNum)
+};
+let userNumber = 50;
+let firstNUm = 10;
+let lastNum = 80;
 
-// if (numberRange(userNumber, firstNUm, lastNum)){
-//     console.log('your number is in the range');
-// } else {
-//    console.log('your number is out of the range')
-// };
+if (numberRange(userNumber, firstNUm, lastNum)){
+    console.log('your number is in the range');
+} else {
+   console.log('your number is out of the range')
+};
 
-// // task 4
-// function isLeapYear(year) {
-//     if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-//       return true; // Leap year
-//     } else {
-//       return false; // Not a leap year
-//     }
-//   }
+// task 4
+function isLeapYear(year) {
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+      return true; // Leap year
+    } else {
+      return false; // Not a leap year
+    }
+  }
   
-//   let userInput = prompt("Enter a year:");
-//   let year = parseInt(userInput);
+  let userInput = prompt("Enter a year:");
+  let year = parseInt(userInput);
   
-//   if (!isNaN(year)) {
-//     if (isLeapYear(year)) {
-//       alert(year + " is a leap year.");
-//     } else {
-//       alert(year + " is not a leap year.");
-//     }
-//   } else {
-//     alert("Invalid input. Please enter a valid year.");
-//   }
+  if (!isNaN(year)) {
+    if (isLeapYear(year)) {
+      alert(year + " is a leap year.");
+    } else {
+      alert(year + " is not a leap year.");
+    }
+  } else {
+    alert("Invalid input. Please enter a valid year.");
+  }
 
 //   task5
 function calculator() {
