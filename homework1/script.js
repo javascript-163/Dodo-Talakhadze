2d463ff07084bf41a8e6f8002e207c3554e68c15
DodoTalakhadze <dtalakhadze2@gmail.com>
2024-02-09 21:55:34 +0400

    omg

--- homework1/script.js
+++ homework1/script.js
@@ -4,3 +4,4 @@ let birthYear = prompt("დაბადების წელი");
 let address = prompt("საცხოვრებელი ადგილი"); 
 let age = 2024-birthYear; 
 alert("გამარჯობა "+ name + " " + surname + ", მიხარია რომ უკვე " + age + " წლის ხარ და " + address + " აქ ცხოვრობ");
+
